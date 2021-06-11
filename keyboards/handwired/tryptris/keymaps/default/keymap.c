fac889e725930b29a1935e75649b3a6e7cc202d6
Mark Story <mark@mark-story.com>
2021-06-10 21:46:44 -0400

    First draft of keymap.

--- keyboards/handwired/tryptris/keymaps/default/keymap.c
+++ keyboards/handwired/tryptris/keymaps/default/keymap.c
@@ -35,19 +35,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* LOWER (symbols and navigation) */
     [_LOWER] = LAYOUT(
-        KC_GRAVE,  KC_1, KC_2, KC_3, KC_4, KC_5,             KC_6, KC_7, KC_8,     KC_9,   KC_0,      KC_BSPACE,
-        KC_TAB,    KC_Q, KC_W, KC_E, KC_R, KC_T,             KC_Y, KC_U, KC_I,     KC_O,   KC_P,      KC_BSLASH,
-        KC_LCTRL,  KC_A, KC_S, KC_D, KC_F, KC_G,             KC_H, KC_J, KC_K,     KC_L,   KC_SCOLON, KC_QUOTE,
-        KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B,             KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH,  KC_RSHIFT,
-               MO(_LOWER), ALT_T(KC_RETURN), GUI_T(KC_ESC),  GUI_T(KC_BSPACE), ALT_T(KC_SPACE), MO(_RAISE)
+        KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,     KC_NO,             KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_TRNS,
+        KC_TRNS, KC_NO, KC_NO, KC_NO,   KC_NO,     KC_NO,             KC_NO,   KC_LCBR, KC_RCBR, KC_LBRC,  KC_RBRC,  KC_TRNS,
+        KC_TRNS, KC_NO, KC_NO, KC_HOME, KC_PGUP,   KC_NO,             KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_EQUAL, KC_TRNS,
+        KC_TRNS, KC_NO, KC_NO, KC_END,  KC_PGDOWN, KC_NO,             KC_NO,   KC_NO,   KC_UNDS, KC_MINS,  KC_PLUS,  KC_TRNS,
+                         KC_TRNS, KC_TRNS, KC_TRNS,                    KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     /* RAISE f-keys, media controls, mouse */
     [_RAISE] = LAYOUT(
-        KC_GRAVE,  KC_1, KC_2, KC_3, KC_4, KC_5,             KC_6, KC_7, KC_8,     KC_9,   KC_0,      KC_BSPACE,
-        KC_TAB,    KC_Q, KC_W, KC_E, KC_R, KC_T,             KC_Y, KC_U, KC_I,     KC_O,   KC_P,      KC_BSLASH,
-        KC_LCTRL,  KC_A, KC_S, KC_D, KC_F, KC_D,             KC_H, KC_J, KC_K,     KC_L,   KC_SCOLON, KC_QUOTE,
-        KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B,             KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH,  KC_RSHIFT,
-               MO(_LOWER), ALT_T(KC_RETURN), GUI_T(KC_ESC),  GUI_T(KC_BSPACE), ALT_T(KC_SPACE), MO(_RAISE)
+        KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_F5,          KC_F6, KC_F7,      KC_F8,      KC_F9,       KC_F10, KC_F11,
+        KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,          KC_NO, KC_NO,      KC_MS_UP,   KC_NO,       KC_NO,  KC_NO,
+        KC_TRNS, KC_MPRV, KC_MNXT, KC_VOLU, KC_NO, KC_NO,          KC_NO, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_NO,  KC_NO,
+        KC_TRNS, KC_MSTP, KC_MPLY, KC_VOLD, KC_NO, KC_NO,          KC_NO, KC_MS_BTN1, KC_BTN2,    KC_BTN3,     KC_NO,  KC_TRNS,
+                         KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
