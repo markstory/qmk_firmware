c94bd683e7b830fb11f8ba3f5cee4ae246a6838b
Mark Story <mark@mark-story.com>
2021-06-25 21:05:04 -0400

    Update keymap
    Having a tap mod on space has not worked out well.

--- keyboards/handwired/tryptris/keymaps/default/keymap.c
+++ keyboards/handwired/tryptris/keymaps/default/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q, KC_W, KC_E, KC_R, KC_T,             KC_Y, KC_U, KC_I,     KC_O,   KC_P,      KC_BSLASH,
         KC_LCTRL,  KC_A, KC_S, KC_D, KC_F, KC_G,             KC_H, KC_J, KC_K,     KC_L,   KC_SCOLON, KC_QUOTE,
         KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B,             KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH,  KC_RSHIFT,
-               MO(_LOWER), ALT_T(KC_RETURN), GUI_T(KC_ESC),  GUI_T(KC_BSPACE), ALT_T(KC_SPACE), MO(_RAISE)
+               MO(_LOWER), ALT_T(KC_ENT), GUI_T(KC_ESC),  ALT_T(KC_BSPACE), KC_SPACE, MO(_RAISE)
     ),
 
     /* LOWER (symbols and navigation) */
